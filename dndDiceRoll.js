1ecbd579a213199eacfd49ac5510498c8bf080bb
Andrew S <andrew.sepuka@gmail.com>
2020-04-19 16:26:26 -0700

    Added logic for when multiple dice are rolled

--- dndDiceRoll.js
+++ dndDiceRoll.js
@@ -12,11 +12,26 @@ function customRoll() {
 let resultArray = [];
 
 function roll(numberOfSides) {
+	let numberOfRolls = document.getElementById("num-of-rolls").value;
+	//initally setting result equal to zero so it may have values added to it
+	let result = 0;
+	//logic for when user is rolling multiple of same dice at once
+	if (numberOfRolls > 1) {
+		let rollCounter = 0;
+		while(rollCounter < numberOfRolls) {
+			result += Math.floor((Math.random() * numberOfSides) + 1);
+			rollCounter++;
+		}
+	}
+	else {
+		result = Math.floor((Math.random() * numberOfSides) + 1);
+	}
 	//have style set to 4em each time in case the user selected help beforehand
 	document.getElementById("result").style.fontSize = "4em";
-	document.getElementById("result").style.color = "#FFBC43"
-	let result = Math.floor((Math.random() * numberOfSides) + 1);
+	document.getElementById("result").style.color = "#FFBC43";
+	//posting result after just in case the styles do not load for whatever reason
 	document.getElementById("result").innerHTML = result;
+
 	//logic for determining if the user had a duplicated result
 	//only a max of two results are kept stored in the array 
 	resultArray.push(result);
@@ -32,16 +47,17 @@ function roll(numberOfSides) {
 //ensures only two results are in the array
 function arrayTrim() {
 	resultArray[0] = resultArray[1];
-	resultArray.splice(-1,1)
+	resultArray.splice(-1,1);
 }
 
 function displayHelp() {
 	//set color of text in case the user had a different color showing beforehand
-	document.getElementById("result").style.color = "#FFBC43"
-	document.getElementById("result").style.fontSize = "1.5em";
+	document.getElementById("result").style.color = "#FFBC43";
+	document.getElementById("result").style.fontSize = "1.4em";
 	document.getElementById("result").innerHTML = "Just click on the button for the corresponding " + 
 	"dice you want to roll. For example, if you want to roll a 6 sided die, click the 'd6' button. " +
 	"To roll a dice with a number of sides not listed, just type your number into the text box and click " + 
 	"custom roll. If you notice the result of your roll change color, that means you rolled the same " +
-	"number again. Good luck!"
+	"number again. To roll multiple dice at once, select a number from the dropdown for how many " + 
+	"dice you want to roll, and click on the type of dice you want to use. Good luck!"
 }
